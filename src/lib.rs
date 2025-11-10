7a938932f8afc2115b8ab1b2a17ab5e42dca0c71
Matheus-git <mathiew0@gmail.com>
2025-11-10 18:53:17 -0300

    append newline

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ use std::net::UdpSocket;
 pub fn send_udp(message: &str) -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
     let destino = "127.0.0.1:8080";
-    socket.send_to(message.as_bytes(), destino)?;
+    socket.send_to(format!("{}\n", message).as_bytes(), destino)?;
     Ok(())
 }
