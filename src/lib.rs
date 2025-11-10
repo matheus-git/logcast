709d6e89ed64249b4bce9bed3d1e8f4dbe078fed
Matheus-git <mathiew0@gmail.com>
2025-11-10 19:27:40 -0300

    add address param

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,12 @@ use std::io;
 use std::net::UdpSocket;
 use chrono::Local;
 
-pub fn send_log(message: &str) -> io::Result<()> {
+pub fn send_log(message: &str, address: &str) -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
-    let destino = "127.0.0.1:8080";
 
     let timestamp = Local::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
     let formatted = format!("\x1b[90m{}\x1b[0m {}\n", timestamp, message);
 
-    socket.send_to(formatted.as_bytes(), destino)?;
+    socket.send_to(formatted.as_bytes(), address)?;
     Ok(())
 }
