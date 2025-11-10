1299da4717a36213854f7d50ae734e6a205ae684
Matheus-git <mathiew0@gmail.com>
2025-11-10 19:04:19 -0300

    add timestamp

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use std::io;
 use std::net::UdpSocket;
+use chrono::Local;
 
 pub fn send_log(message: &str) -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
     let destino = "127.0.0.1:8080";
-    socket.send_to(format!("{}\n", message).as_bytes(), destino)?;
+
+    let timestamp = Local::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
+    let formatted = format!("{} {}\n", timestamp, message);
+
+    socket.send_to(formatted.as_bytes(), destino)?;
     Ok(())
 }
