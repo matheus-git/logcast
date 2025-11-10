488c59eb9bb620521a1cabb9fcaad0dc66d34ca4
Matheus-git <mathiew0@gmail.com>
2025-11-10 18:56:14 -0300

    change function name

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::net::UdpSocket;
 
-pub fn send_udp(message: &str) -> io::Result<()> {
+pub fn send_log(message: &str) -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
     let destino = "127.0.0.1:8080";
     socket.send_to(format!("{}\n", message).as_bytes(), destino)?;
