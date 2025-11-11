c0ab02c91bbad13e66bae07ab351ec1b51ebb6fb
Matheus-git <mathiew0@gmail.com>
2025-11-10 21:13:01 -0300

    change to tcp

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-use std::io;
-use std::net::UdpSocket;
+use std::io::{self, Write};
+use std::net::TcpStream;
 use chrono::Local;
 
 pub fn send_log(message: &str, address: &str) -> io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    let mut stream = TcpStream::connect(address)?;
 
     let timestamp = Local::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
     let formatted = format!("\x1b[90m{}\x1b[0m {}\n", timestamp, message);
 
-    socket.send_to(formatted.as_bytes(), address)?;
+    stream.write_all(formatted.as_bytes())?;
     Ok(())
 }
